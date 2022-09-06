58cb47b289e0063832eccee358a3787a2246b057
Joaquin <joaquin.busquistar@gmail.com>
2022-09-06 16:07:50 +0200

    Cambiar titulo con input

--- js/index.js
+++ js/index.js
@@ -1,24 +1,8 @@
-let title = document.getElementById("title");
-let div = document.getElementById("container");
-let header = document.getElementById("cabecera");
-let footer = document.getElementById("pie");
-let image = document.getElementById("imagen");
+const title = document.getElementById("title");
+const header = document.getElementById("cabecera");
 
-let parrafo = document.createElement("p");
-parrafo.innerHTML = "Se trata de un parrafo";
 
-div.appendChild(parrafo);
-
-let h2 = document.createElement("h2")
-
-header.onclick = () => {
-    h2.innerHTML = "Se trata de un subtitulo de calibre 2"
-    div.appendChild(h2)
+function myFunction() {
+    let x = document.getElementById("input").value
+    document.getElementById("title").innerHTML = x
 }
-
-footer.onclick = () => {
-    div.removeChild(h2);
-}
-
-image.setAttribute("width", "100px")
-image.setAttribute("height", "100px")
